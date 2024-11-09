7f9b6e0ef81dfece62d74d9414484afaf4d8d0d9
Komal Nadeem <koomalkhan111@gmail.com>
2024-11-09 17:42:39 +0500

    Add files via upload

---
+++ assignment chap 4/app.js
@@ -0,0 +1,24 @@
+// QUESTION NO 1:
+
+let var1 = 15, var2 = 19, var3 = 10
+
+
+// QUESTION NO 2:
+// LEGAL VARIABLE NAME
+
+let firstName;
+let _userData;
+let $amount;
+let myVar123;
+let total_Amount;
+
+// ILLEGAL VARIABLE NAME
+
+// let 123abc;
+// let first name;
+// let return;
+// let first-name;
+// let Math;
+
+// QUESTION NO 3:
+//  IN HTML PAGE
